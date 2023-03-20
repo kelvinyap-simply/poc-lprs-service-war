bcb4f6640f3a4c50981e548b5cc83c4b321d2893
Kelvin Yap <kelvin.yap@simplysakal.asia>
2023-03-20 11:27:27 +0800

    Add servlet

--- src/main/java/com/simply/poclprsservicewar/PocLprsServiceWarApplication.java
+++ src/main/java/com/simply/poclprsservicewar/PocLprsServiceWarApplication.java
@@ -2,6 +2,8 @@ package com.simply.poclprsservicewar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,10 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class PocLprsServiceWarApplication {
+public class PocLprsServiceWarApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PocLprsServiceWarApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PocLprsServiceWarApplication.class);
 	}
 
 	//@GetMapping("/hello")
