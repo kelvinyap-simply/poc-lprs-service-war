aa13cf0c83180a1a43330cd43d97184ee4d3f14e
Kelvin Yap <kelvin.yap@simplysakal.asia>
2023-03-20 11:08:35 +0800

    Add controller

---
+++ src/main/java/com/simply/poclprsservicewar/PocLprsServiceWarApplication.java
@@ -0,0 +1,31 @@
+package com.simply.poclprsservicewar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class PocLprsServiceWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocLprsServiceWarApplication.class, args);
+	}
+
+	//@GetMapping("/hello")
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String hello() {
+		return "Hello World";
+	}
+
+	//@PostMapping("/api/eventReceiver")
+	@RequestMapping(value = "/api/eventReceiver", method = RequestMethod.POST)
+	public String eventListener(@RequestBody String msg) {
+		System.out.println("Message: " + msg);
+		return msg;
+	}
+
+}
---
+++ src/test/java/com/simply/poclprsservicewar/PocLprsServiceWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simply.poclprsservicewar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocLprsServiceWarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
